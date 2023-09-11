854b49ddafa4b04f57d152fa5fca83b601010678
Md Md Farid <MdFaridMA@cardiff.ac.uk>
2023-09-11 11:52:28 +0800

    style: Modifying Java file to keep code clean

--- Java_Projects/TheJavaPracticeZone.java
+++ Java_Projects/TheJavaPracticeZone.java
@@ -1,5 +1,7 @@
 class TheJavaPracticeZone{
 	
+	// This program is a playground to allow programming experimentation.
+	
 	public static void oddOrEven(int a) {
 		if (a % 2 == 0) {
 			System.out.println("Even");
